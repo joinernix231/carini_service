ba79e722c1b149f61d46924393b6eb0aad4557b2
David Martinez <davidalejandromartinezpinzon@gmail.com>
2025-09-12 00:20:46 -0500

    PERRO cree el metodo index para leer la informaacion de los coordinadores/Coordinator tambien cree el metodo READCOORDINATORSAPIREQUES(bien escrito kasjdkajs como lo tiene en su estructura le puse que retornara en TRUE para que no me denegara el acceso tambien en el postman o como se llame en el header puse una llave de autorizacion para que con el token me dejara acceder a funciones porque estaba probando el metodo get y no me dejaba y ya bro mañana hago los otros metodos del CRUD para coordinators

--- app/Http/Controllers/Coordinator/CoordinatorAPIController.php
+++ app/Http/Controllers/Coordinator/CoordinatorAPIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Coordinator;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Client\ReadClientAPIRequest;
 use App\Http\Requests\Coordinator\CreateCoordinatorAPIRequest;
 use App\Http\Resources\Coordinator\CoordinatorResource;
 use App\Repositories\Coordinator\CoordinatorRepository;
@@ -12,6 +13,11 @@ class CoordinatorController extends Controller
 {
     public function __construct(private readonly CoordinatorRepository $coordinatorRepository)
     {}
+
+    public function index(ReadCoordinatorAPIRequest)
+    {
+
+    }
 
     public function store(CreateCoordinatorAPIRequest $request)
     {
